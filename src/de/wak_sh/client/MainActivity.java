17b384ffb5c0390408ea148e2ba1f00a74bedd3b
Christoph Mende <mende.christoph@gmail.com>
2013-09-18 10:35:09 +0200

    Add stub fragments and ViewPager in MainActivity

--- src/de/wak_sh/client/MainActivity.java
+++ src/de/wak_sh/client/MainActivity.java
@@ -1,15 +1,31 @@
 package de.wak_sh.client;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.viewpagerindicator.TabPageIndicator;
 
+import de.wak_sh.client.fragments.NachrichtenFragment;
+import de.wak_sh.client.fragments.NotenFragment;
+
+public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		SectionPagerAdapter pagerAdapter = new SectionPagerAdapter(
+				getSupportFragmentManager(), this);
+		pagerAdapter.addFragment(new NachrichtenFragment());
+		pagerAdapter.addFragment(new NotenFragment());
+
+		ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+		viewPager.setAdapter(pagerAdapter);
+
+		TabPageIndicator indicator = (TabPageIndicator) findViewById(R.id.pager_indicator);
+		indicator.setViewPager(viewPager);
 	}
 
 	@Override
---
+++ src/de/wak_sh/client/fragments/NachrichtenFragment.java
@@ -0,0 +1,20 @@
+package de.wak_sh.client.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import de.wak_sh.client.R;
+
+public class NachrichtenFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_nachrichten,
+				container, false);
+
+		return rootView;
+	}
+
+}
