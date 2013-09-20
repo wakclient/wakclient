fc859d0fb1f9d84336a1509649cdf5802b99089b
Christoph Mende <mende.christoph@gmail.com>
2013-09-20 11:18:49 +0200

    Add DataFragment interface so MainActivity can tell its children to fetch their data when they're displayed

--- src/de/wak_sh/client/MainActivity.java
+++ src/de/wak_sh/client/MainActivity.java
@@ -2,8 +2,11 @@ package de.wak_sh.client;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,11 +14,25 @@ import com.viewpagerindicator.TabPageIndicator;
 
 import de.wak_sh.client.backend.DataService;
 import de.wak_sh.client.fragments.BenutzerinfoFragment;
+import de.wak_sh.client.fragments.DataFragment;
 import de.wak_sh.client.fragments.NachrichtenFragment;
 import de.wak_sh.client.fragments.NotenFragment;
 
 public class MainActivity extends FragmentActivity {
 	public static final String ACTION_LOGOUT = "de.wak_sh.client.ACTION_LOGOUT";
+
+	private ViewPager viewPager;
+	private SectionPagerAdapter pagerAdapter;
+
+	private OnPageChangeListener onPageChangeListener = new SimpleOnPageChangeListener() {
+		@Override
+		public void onPageSelected(int position) {
+			Fragment fragment = pagerAdapter.getItem(position);
+			if (fragment instanceof DataFragment) {
+				((DataFragment) fragment).fetchData();
+			}
+		};
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +42,25 @@ public class MainActivity extends FragmentActivity {
 			Intent intent = new Intent(this, LoginActivity.class);
 			startActivity(intent);
 			finish();
+			return;
 		}
 
 		setContentView(R.layout.activity_main);
 
-		SectionPagerAdapter pagerAdapter = new SectionPagerAdapter(
-				getSupportFragmentManager(), this);
+		pagerAdapter = new SectionPagerAdapter(getSupportFragmentManager(),
+				this);
 		pagerAdapter.addFragment(new BenutzerinfoFragment());
 		pagerAdapter.addFragment(new NachrichtenFragment());
 		pagerAdapter.addFragment(new NotenFragment());
 
-		ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+		viewPager = (ViewPager) findViewById(R.id.pager);
 		viewPager.setAdapter(pagerAdapter);
 
 		TabPageIndicator indicator = (TabPageIndicator) findViewById(R.id.pager_indicator);
+		indicator.setOnPageChangeListener(onPageChangeListener);
 		indicator.setViewPager(viewPager);
+
+		onPageChangeListener.onPageSelected(viewPager.getCurrentItem());
 	}
 
 	@Override
---
+++ src/de/wak_sh/client/fragments/DataFragment.java
@@ -0,0 +1,5 @@
+package de.wak_sh.client.fragments;
+
+public interface DataFragment {
+	void fetchData();
+}
