63d972d7c0fc18792731496fc2ae6bdb60766dbc
Christoph Mende <mende.christoph@gmail.com>
2013-09-24 21:55:51 +0200

    Fix regex utils for long messages

--- src/de/wak_sh/client/Utils.java
+++ src/de/wak_sh/client/Utils.java
@@ -7,14 +7,16 @@ import java.util.regex.Pattern;
 
 public class Utils {
 	public static String match(String pattern, String subject) {
-		Matcher matcher = Pattern.compile(pattern).matcher(subject);
+		Matcher matcher = Pattern.compile(pattern, Pattern.DOTALL).matcher(
+				subject);
 		matcher.find();
 		return matcher.group(1);
 	}
 
 	public static List<String[]> matchAll(String pattern, String subject) {
 		List<String[]> matches = new ArrayList<String[]>();
-		Matcher matcher = Pattern.compile(pattern).matcher(subject);
+		Matcher matcher = Pattern.compile(pattern, Pattern.DOTALL).matcher(
+				subject);
 		while (matcher.find()) {
 			int count = matcher.groupCount();
 			String[] match = new String[count];
