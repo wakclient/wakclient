4287413f6cde008fa8899bf2c4c5be07339d3f72
Christoph Mende <mende.christoph@gmail.com>
2013-10-02 10:09:15 +0200

    Use ListView header instead of defining the header in the surrounding layout

--- src/de/wak_sh/client/fragments/SemesterFragment.java
+++ src/de/wak_sh/client/fragments/SemesterFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 import de.wak_sh.client.R;
 import de.wak_sh.client.backend.adapters.ModuleArrayAdapter;
 import de.wak_sh.client.backend.model.Module;
@@ -24,6 +25,22 @@ public class SemesterFragment extends Fragment {
 				.getSerializable("grades");
 
 		ListView list = (ListView) rootView.findViewById(android.R.id.list);
+
+		View header = inflater.inflate(R.layout.noten_list_item, null);
+
+		TextView moduleName = (TextView) header.findViewById(R.id.txt_modul);
+		TextView credits = (TextView) header.findViewById(R.id.txt_credits);
+		TextView grade1 = (TextView) header.findViewById(R.id.txt_note1);
+		TextView grade2 = (TextView) header.findViewById(R.id.txt_note2);
+		TextView grade3 = (TextView) header.findViewById(R.id.txt_note3);
+
+		moduleName.setText(R.string.module);
+		credits.setText(R.string.credits);
+		grade1.setText(R.string.grade1);
+		grade2.setText(R.string.grade2);
+		grade3.setText(R.string.grade3);
+
+		list.addHeaderView(header);
 		list.setAdapter(new ModuleArrayAdapter(getActivity(), grades));
 
 		return rootView;
