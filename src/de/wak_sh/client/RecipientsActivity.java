6f8a3e312fae623fcd7beee632bb0886a30a8a33
Artem Hofmann <hofmann.artem@gmail.com>
2014-01-31 21:00:35 +0100

    Implement RecipientActivity

--- src/de/wak_sh/client/RecipientsActivity.java
+++ src/de/wak_sh/client/RecipientsActivity.java
@@ -1,43 +1,56 @@
 package de.wak_sh.client;
 
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.os.AsyncTask;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.widget.ListView;
 
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.widget.SearchView;
+import com.actionbarsherlock.widget.SearchView.OnQueryTextListener;
 
 import de.wak_sh.client.backend.AdapterRecipients;
+import de.wak_sh.client.backend.ProgressTask;
+import de.wak_sh.client.backend.SelectionDialogRecipient;
+import de.wak_sh.client.backend.SelectionDialogRecipient.OnRecipientSelectedListener;
 import de.wak_sh.client.model.Recipient;
+import de.wak_sh.client.service.JsoupEmailService;
 
-public class RecipientsActivity extends SherlockActivity {
+public class RecipientsActivity extends SherlockFragmentActivity implements
+		OnQueryTextListener, OnRecipientSelectedListener {
 
-	public static final int REQUEST_CODE = 1;
-
+	private ListView listView;
 	private List<Recipient> mRecipients = new ArrayList<Recipient>();
-	private AdapterRecipients mAdapter;
 
 	private SearchView searchView;
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recipients);
 
-		ListView listView = (ListView) findViewById(android.R.id.list);
-		mAdapter = new AdapterRecipients(this, mRecipients);
-		listView.setAdapter(mAdapter);
+		mRecipients = (List<Recipient>) getIntent().getExtras()
+				.getSerializable("list");
+
+		listView = (ListView) findViewById(android.R.id.list);
+		updateList();
 	}
 
 	@Override
 	public void onBackPressed() {
-
-		finish();
+		Intent data = new Intent();
+		data.putExtra("list", (Serializable) mRecipients);
+		setResult(RESULT_OK, data);
+		super.onBackPressed();
 	}
 
 	@Override
@@ -45,26 +58,66 @@ public class RecipientsActivity extends SherlockActivity {
 		getSupportMenuInflater().inflate(R.menu.recipients, menu);
 		MenuItem item = menu.findItem(R.id.action_search);
 		searchView = (SearchView) item.getActionView();
+		searchView.setOnQueryTextListener(this);
 		return true;
 	}
 
-	private class SearchTask extends AsyncTask<Void, Void, Void> {
-
-		@Override
-		protected void onPreExecute() {
-
-		}
-
-		@Override
-		protected Void doInBackground(Void... params) {
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Void result) {
-
-		}
-
+	@Override
+	public boolean onQueryTextSubmit(String query) {
+		new SearchTask(this, null, "Suche Empfänger...").execute(query);
+		searchView.clearFocus();
+		return true;
 	}
 
+	@Override
+	public boolean onQueryTextChange(String newText) {
+		return false;
+	}
+
+	@Override
+	public void onRecipientSelected(DialogFragment dialog, Recipient recipient) {
+		if (!mRecipients.contains(recipient)) {
+			mRecipients.add(recipient);
+			updateList();
+		}
+
+		searchView.setQuery("", false);
+		searchView.requestFocus();
+		dialog.dismiss();
+	}
+
+	private void updateList() {
+		listView.setAdapter(new AdapterRecipients(this, mRecipients));
+	}
+
+	private class SearchTask extends
+			ProgressTask<String, Void, List<Recipient>> {
+
+		public SearchTask(Context context, String title, String message) {
+			super(context, title, message);
+		}
+
+		@Override
+		protected List<Recipient> doInBackground(String... params) {
+			try {
+				return JsoupEmailService.getInstance().getRecipients(params[0]);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+
+		@Override
+		protected void onPostExecute(List<Recipient> result) {
+			super.onPostExecute(result);
+			if (result != null) {
+				Bundle bundle = new Bundle();
+				bundle.putSerializable("list", (Serializable) result);
+
+				SelectionDialogRecipient dialog = new SelectionDialogRecipient();
+				dialog.setArguments(bundle);
+				dialog.show(getSupportFragmentManager(), null);
+			}
+		}
+	}
 }
--- src/de/wak_sh/client/model/Recipient.java
+++ src/de/wak_sh/client/model/Recipient.java
@@ -41,6 +41,49 @@ public class Recipient implements Serializable {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result
+				+ ((location == null) ? 0 : location.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Recipient other = (Recipient) obj;
+		if (id != other.id) {
+			return false;
+		}
+		if (location == null) {
+			if (other.location != null) {
+				return false;
+			}
+		} else if (!location.equals(other.location)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Recipient [id=" + id + ", name=" + name + ", location="
 				+ location + "]";
